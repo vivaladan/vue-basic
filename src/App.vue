0a53b396e2dad4da0c377b70fe812232cb30d655
Daniel Revell <mail@danielrevell.com>
2021-06-05 22:28:00 +0100

    show login

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>
+    <div>Hello {{ value }}</div>
+    <template v-if="userName">
+      <div>Hi {{ userName }} <a href=".auth/logout/aad">Logout</a></div>
+    </template>
+    <template v-else>
+      <div>Not logged in <a href=".auth/login/aad">Login</a></div>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -8,12 +16,20 @@ export default {
   data() {
     return {
       value: "World",
+      userName: "",
     };
   },
   async mounted() {
+    const userResponse = await fetch(".auth/me");
+    const {
+      clientPrincipal: { userDetails },
+    } = await userResponse.json();
+    this.userName = userDetails;
+
     const response = await fetch("api/message");
     const { text } = await response.json();
     this.value = text;
   },
+  components: {},
 };
 </script>
